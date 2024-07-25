a3e23056d1ce5757a19ea265d9e225cafae3a200
Idris Khattak <idriskhan999999@gmail.com>
2024-07-25 12:01:27 +0500

    added code for dictionary

---
+++ Dictionary.py
@@ -0,0 +1,18 @@
+# Include Key, Value pairs
+# Mutable, Unordered
+
+Dict = {
+    'name':'Idris',
+    'age': 24,
+    'height' : 5.8
+}
+
+print(Dict)
+
+Dict['name'] = 'Khan' # ---> New value assin with the old key
+print(Dict)
+
+Dict['cast'] = 'Khattak' # ---> New key added in the same dict
+print(Dict)
+
+print(Dict['name']) # ---> Printing value using key
---
+++ Dictionary_Nested.py
@@ -0,0 +1,21 @@
+# A dictionary inside a dictionary
+
+Dict = {
+    'name':{
+        'stnd_1': 'Idris',
+        'stnd_2': 'Khan',
+        'stnd_3': 'Khattak'
+    },
+    'grades':{
+        'stnd_1': 'A-',
+        'stnd_2': 'B+',
+        'stnd_3': 'A+'
+    }
+}
+
+print(Dict['name']['stnd_1'])
+
+# Dictionay Methods
+print(Dict.keys()) # Priting just the keys in Dict
+print(Dict.values())  # Priting just the values in Dict
+print(Dict.items())  # Priting both the keys and values in Dict
